6400dfbc3100cf035e02612ab1ca4ef9ea08b2ce
frogonhills <shifat.ahmed@northsouth.edugit config --global user.email shifat.ahmed@northsouth.edugit config --global user.email shifat.ahmed@northsouth.edu>
2021-01-14 20:07:04 +0600

    larv

---
+++ src_extends/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import {name , age , person} from './data';
+
+import { Component } from 'react';
+import Banner from './components/Banner';
+
+
+class App extends Component{
+  render(){
+    return(
+
+      <section>
+
+        <Banner />
+        
+      </section>
+
+    );
+  }
+}
+
+
+
+
+export default App ;
+
+
+
+
+
+
